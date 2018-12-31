707004c0ff8df785a206eef28e4a14b64fda6b9e
Andrew Morcom <jubulani@fastmail.fm>
2019-01-01 00:20:14 +0200

    Tests of throwing errors from backend

--- renderer/main.ts
+++ renderer/main.ts
@@ -6,8 +6,12 @@ let btn = document.getElementById('file-selector');
 btn.addEventListener('click', (e:Event) => dialog.showOpenDialog(
     { properties: ['openFile', 'openDirectory',] },
     (filepaths) => {
-    // Only pass the first filepath for now
-        Backend.openFile(filepaths[0]);
+        try {
+            // Only pass the first filepath for now
+            Backend.openFile(filepaths[0]);
+        } catch (err) {
+            alert(err);
         }
+    }
     )
 );

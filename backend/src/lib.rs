1018ddd14b86dfd78ce031ba03771332e0de263e
Andrew Morcom <jubulani@fastmail.fm>
2019-03-08 11:01:25 +0200

    Minor changes to add wasm_bindgen

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,3 +1,16 @@
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("👋 from Wasm");
+}
+
 #[cfg(test)]
 mod tests {
     #[test]

ac78206cd52a790c586008c1317e0ab3ec2be6bf
Andrew Morcom <jubulani@fastmail.fm>
2019-03-10 16:53:38 +0200

    Add webpack-dev-server.  It doesn't seem to really work, but it's necessary for serving wasm in dev

--- web/ts/main.ts
+++ web/ts/main.ts
@@ -1,1 +1,12 @@
 alert('This is Typescript');
+
+function start(mymod: typeof import("../../backend/pkg/backend_bg")) {
+    console.log("All modules loaded");
+    mymod.greet();
+}
+
+async function load() {
+    start(await import("../../backend/pkg/backend_bg"));
+}
+
+load();

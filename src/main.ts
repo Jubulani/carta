b70085b3d064b825e55ea5ae0d721b6082298e04
Andrew Morcom <jubulani@fastmail.fm>
2019-03-11 19:19:27 +0200

    Minoor event handler change

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,17 @@ function start_wasm(mymod: typeof import("../wasm/pkg/carta_backend_bg")) {
     console.log("All wasm modules loaded");
     mymod.greet();
 }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    console.log('Document loaded');
+    let fileElem = document.getElementById('file-upload');
+    if (fileElem) {
+        fileElem.addEventListener('change', readFiles);
+    } else {
+        console.log('Could not find file upload element');
+    }
+});
+
+function readFiles(event: Event) {
+    console.log('Read file event');
+}

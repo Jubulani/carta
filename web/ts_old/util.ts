6dc2e95ca5ed5668bff6f03176f248ec89c6095a
Andrew Morcom <jubulani@fastmail.fm>
2019-03-16 21:25:44 +0200

    Remove old src files

--- web/ts_old/util.ts
+++
@@ -1,6 +0,0 @@
-export function removeClassFromElement(elementTag: string, _class: string) {
-    let elems = document.getElementsByTagName(elementTag);
-    for (let i = 0; i < elems.length; ++i) {
-        elems[i].classList.remove(_class);
-    }
-}

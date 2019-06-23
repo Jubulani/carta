aeca7968bfad01d1415da1e9140fee32009917bc
Andrew Morcom <jubulani@fastmail.fm>
2019-06-23 22:56:11 +0300

    Ability to minimise/maximise schema

--- src/binfiles.ts
+++ src/binfiles.ts
@@ -6,6 +6,7 @@ class BinFile {
     filename: string;
     file_data_div: Element;
     unique_id: string;
+    nugget: any = null;
 
     constructor(filename: string, file_data: ArrayBuffer, file_idx: number) {
         this.data = new Uint8Array(file_data);
@@ -16,21 +17,20 @@ class BinFile {
     }
 
     apply_schema() {
-        // Remove the schema display if we have one, so we can display a new one
-        let schema_data_divs = this.file_data_div.getElementsByClassName("schema-data");
-        if (schema_data_divs.length > 0) {
-            schema_data_divs[0].remove();
-        }
-
         try {
-            let nugget = schema.apply_schema(this.data);
-            if (nugget) {
-                display_schema_data(this.file_data_div, nugget);
+            this.nugget = schema.apply_schema(this.data);
+            if (this.nugget) {
+                set_nugget_ids(this.nugget, 0);
             }
+            this.display_schema();
         }
         catch (err) {
             alert(err);
         }
+    }
+
+    display_schema() {
+        display_schema_data(this.file_data_div, this.nugget);
     }
 }
 
@@ -42,9 +42,7 @@ export function new_file(filename: string, file_data: ArrayBuffer) {
 }
 
 export function apply_new_schema() {
-    for (let i = 0; i < all_files.length; ++i) {
-        all_files[i].apply_schema();
-    }
+    all_files.forEach(e => e.apply_schema());
 }
 
 function display_file(filename: string, data: Uint8Array, unique_id: string): Element {
@@ -123,20 +121,29 @@ function byte_to_ascii(b: number): string {
 }
 
 function display_schema_data(file_data_div: Element, nugget: any) {
-    let schema_data_div = append_div_with_class(file_data_div, "schema-data");
-    schema_data_div.innerHTML = get_root_schema_data(nugget);
-    update_schema_event_listeners();
+    // Remove the schema display if we have one, so we can display a new one
+    let schema_data_divs = file_data_div.getElementsByClassName("schema");
+    for (let i = 0; i < schema_data_divs.length; ++i) {
+        schema_data_divs[i].remove();
+    }
+
+    if (nugget) {
+        let schema_div = append_div_with_class(file_data_div, "schema");
+
+        let schema_minmax_div = append_div_with_class(schema_div, "schema-minmax");
+        schema_minmax_div.innerHTML = get_root_minmax_buttons(nugget);
+
+        let schema_data_div = append_div_with_class(schema_div, "schema-data");
+        schema_data_div.innerHTML = get_root_schema_data(nugget);
+
+        update_schema_event_listeners(file_data_div);
+    }
 }
 
 var nugget_id = 0;
 function get_nugget_text(nugget: any, depth: number): string {
     let value = new Array(depth + 1).join('  ');
     value += `<span class=schema-nugget data-idx-start=${nugget.start} data-idx-len=${nugget.len} data-nugget-id=${nugget_id++}>`;
-    if (nugget.children.length > 0) {
-        value += `<span class="far fa-minus-square"></span> `;
-    } else {
-        value += '  ';
-    }
     value += nugget.name;
     if (nugget.value) {
         value += `: ${nugget.value}`;
@@ -146,10 +153,14 @@ function get_nugget_text(nugget: any, depth: number): string {
 
 function get_schema_data(nugget: any, depth: number): string {
     let schema_data = get_nugget_text(nugget, depth);
-    return nugget.children.reduce(
-        (accu: string, val: any) => accu += get_schema_data(val, depth + 1),
-        schema_data,
-    );
+    if (nugget.minimised) {
+        return schema_data;
+    } else {
+        return nugget.children.reduce(
+            (accu: string, val: any) => accu += get_schema_data(val, depth + 1),
+            schema_data,
+        );
+    }
 }
 
 function get_root_schema_data(nugget: any): string {
@@ -159,12 +170,155 @@ function get_root_schema_data(nugget: any): string {
     );
 }
 
-function update_schema_event_listeners() {
-    let elems = document.getElementsByClassName('schema-nugget');
+function get_minmax_data(nugget: any) {
+    if (nugget.children.length) {
+        if (nugget.minimised) {
+            return `<span class="maximise" data-nugget-id="${nugget.id}"><i class="far fa-plus-square"></i></span>\n`;
+        } else {
+            return nugget.children.reduce(
+                (accu: string, val: any) => accu += get_minmax_data(val),
+                `<span class="minimise" data-nugget-id="${nugget.id}"><i class="far fa-minus-square"></i></span>\n`
+            );
+        }
+    } else {
+        return '\n';
+    }
+}
+
+function get_root_minmax_buttons(nugget: any): string {
+    return nugget.children.reduce(
+        (accu: string, val: any) => accu += get_minmax_data(val),
+        ''
+    );
+}
+
+function get_span_parent(elem: Element): Element | null {
+    // Might have a path or svg element from fontawesome instead of the span that we need
+    // If so, get the parent or parent's parent.  We might need to go path -> svg -> span
+    let result: Element | null = elem;
+    if (elem.tagName.toLowerCase() !== 'span') {
+        result = elem.parentElement;
+    }
+    if (result && result.tagName.toLowerCase() !== 'span') {
+        result = result.parentElement;
+    }
+    return result;
+}
+
+function click_minimise(e: Event) {
+    // Make sure e.target is an Element for typescript
+    if (!(e.target instanceof Element)) {
+        console.error('Expected element, got ' + e.target);
+        return;
+    }
+    let target = get_span_parent(e.target);
+    if (!target) {
+        console.error('Could not find parent span element');
+        return;
+    }
+
+    let nugget_id_attr = target.attributes.getNamedItem('data-nugget-id');
+    if (!nugget_id_attr) {
+        console.error('data-nugget-id attribute not found');
+        return;
+    }
+    let nugget_id = parseInt(nugget_id_attr.value);
+    minimise_nugget_from_span(nugget_id, target, true);
+}
+
+function click_maximise(e: Event) {
+    // Make sure e.target is an Element for typescript
+    if (!(e.target instanceof Element)) {
+        console.error('Expected element, got ' + e.target);
+        return;
+    }
+    let target = get_span_parent(e.target);
+    if (!target) {
+        console.error('Could not find parent span element');
+        return;
+    }
+
+    let nugget_id_attr = target.attributes.getNamedItem('data-nugget-id');
+    if (!nugget_id_attr) {
+        console.error('data-nugget-id attribute not found');
+        return;
+    }
+    let nugget_id = parseInt(nugget_id_attr.value);
+    minimise_nugget_from_span(nugget_id, target, false);
+}
+
+function minimise_nugget_from_span(nugget_id: number, clicked_elem: Element, min_value: boolean) {
+    // Find the unique file-id.  Go up the parent divs until we reach the file-container,
+    // checking each one as we go
+    let p1 = clicked_elem.parentElement;
+    if (!p1 || !p1.classList.contains('schema-minmax')) {
+        console.error('Could not find schema-minmax div');
+        return;
+    }
+
+    let p2 = p1.parentElement;
+    if (!p2 || !p2.classList.contains('schema')) {
+        console.error('Could not find schema div');
+        return;
+    }
+
+    let p3 = p2.parentElement;
+    if (!p3 || !p3.classList.contains('file-data')) {
+        console.error('Could not find file-date div');
+        return;
+    }
+
+    let p4 = p3.parentElement;
+    if (!p4 || !p4.classList.contains('file-container')) {
+        console.error('Could not find file-container div');
+        return;
+    }
+
+    let file_id = p4.id;
+    const binfile = all_files.find(file => file.unique_id === file_id);
+    if (!binfile) {
+        console.error('Could not find appropriate BinFile object');
+        return;
+    }
+
+    minimise_nugget(nugget_id, binfile.nugget, min_value);
+    binfile.display_schema();
+}
+
+function minimise_nugget(nugget_id: number, nugget: any, min_value: boolean) {
+    // Find the appropriuate nugget to minimise
+    if (nugget.id === nugget_id) {
+        nugget.minimised = min_value;
+        return;
+    } else if (nugget.children.length) {
+        for (let i = 0; i < nugget.children.length; ++i) {
+            if (nugget.children.id > nugget_id) {
+                return;
+            } else if (i < nugget.children.length - 1 && nugget.children[i + 1].id < nugget_id) {
+                // Can't be in this child, skip to the next one
+                continue;
+            } else {
+                minimise_nugget(nugget_id, nugget.children[i], min_value);
+            }
+        }
+    }
+}
+
+function update_schema_event_listeners(file_data_div: Element) {
+    const elems = file_data_div.getElementsByClassName('schema-nugget');
     Array.from(elems).forEach(function (elem) {
         elem.addEventListener('mouseenter', nugget_mouseenter);
         elem.addEventListener('mouseleave', nugget_mouseleave);
-    })
+    });
+
+    const min_elems = file_data_div.getElementsByClassName("minimise");
+    Array.from(min_elems).forEach(elem => {
+        elem.addEventListener('click', click_minimise);
+    });
+    const max_elems = file_data_div.getElementsByClassName("maximise");
+    Array.from(max_elems).forEach(elem => {
+        elem.addEventListener('click', click_maximise);
+    });
 }
 
 function nugget_mouseleave(e: Event) {
@@ -224,9 +378,8 @@ function nugget_mouseenter(e: Event) {
 function highlight_hexdata(start: number, len: number, parent: Element) {
     // '*2 '    - Each byte is composed of two displayed chars
     // '* 1.25' - Each block of four displayed chars is followed by a single separator char
-    // '- 0.01' - If the end char is a separator, don't include it
     let start_char = Math.floor((start * 2) * 1.25);
-    let end_char = Math.floor(((start + len) * 2) * 1.25 - 0.01);
+    let end_char = Math.floor(((start + len) * 2) * 1.25);
 
     let hex_elem = parent.getElementsByClassName('hex-data')[0];
     let hex_text = hex_elem.textContent;
@@ -259,4 +412,14 @@ function highlight_asciidata(start: number, len: number, parent: Element) {
     let post = ascii_text.substring(end_char);
 
     ascii_elem.innerHTML = `${pre}<span class="highlight">${highlight}</span>${post}`;
+}
+
+function set_nugget_ids(nugget: any, id: number): number {
+    nugget.id = id++;
+    id = nugget.children.reduce((accu: number, n: any) => {
+        return set_nugget_ids(n, accu);
+    },
+        id
+    );
+    return id;
 }
